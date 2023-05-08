fbec988d566ebb86ebdd2678f84d0a318381ffac
Your Name <129026648+Vneet11@users.noreply.github.com>
2023-05-08 15:21:20 -0700

    completed user model

--- ninety-nine-cats2/app/models/user.rb
+++ ninety-nine-cats2/app/models/user.rb
@@ -1,4 +1,35 @@
 class User < ApplicationRecord
     validates :username, :session_token, uniqueness: true, presence: true
     validates :password_digest, presence: true
+
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+        if user && user.is_valid_password?
+            user 
+        else 
+            nil 
+        end 
+    end 
+
+    def password=(password)
+        @password = password 
+        self.password_digest = BCrypt::Password.create(password)
+    end 
+
+    def is_valid_password?(password)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
+    end
+
+    def reset_session_token!
+        self.session_token = SecureRandom::url_safe64
+        self.save!
+        self.session_token
+    end 
+
+    def ensure_session_token
+        self.session_token ||= SecureRandom::url_safe64
+    end 
+
+
 end
