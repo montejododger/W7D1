f84954430994b90fbcc53df419048ffab2b92854
Your Name <129026648+Vneet11@users.noreply.github.com>
2023-05-08 16:34:13 -0700

    updated controllers

--- ninety-nine-cats2/app/controllers/application_controller.rb
+++ ninety-nine-cats2/app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+    before_action :require_logged_in, only: [:show, :edit, :update]
+    before_action :require_logged_out, only: [:new, :create]
+
 end
--- ninety-nine-cats2/app/controllers/users_controller.rb
+++ ninety-nine-cats2/app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+    # before_action :require_logged_in, only: [:show, :edit, :update]
+    # before_action :require_logged_out, only: [:new, :create]
+
+    
+    def new
+        @user = User.new
+        render :new 
+    end 
+
+    def create
+        @user = User.new(user_params)
+        if @user.save 
+            login!(@user)
+            redirect_to cats_url
+        else 
+            render :new 
+        end 
+
+    end 
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:username, :session_token, :password_digest)
+
+    end 
 end
