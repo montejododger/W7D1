19e49eba6993ffabdeb5b3fcb830310aa3051f35
Matthew Montejo <montejododger@yahoo.com>
2023-05-08 15:40:15 -0700

    routes added

--- ninety-nine-cats2/app/models/user.rb
+++ ninety-nine-cats2/app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
     validates :username, :session_token, uniqueness: true, presence: true
     validates :password_digest, presence: true
 
+    attr_reader :password
+
     def self.find_by_credentials(username, password)
         user = User.find_by(username: username)
         if user && user.is_valid_password?
@@ -22,14 +24,24 @@ class User < ApplicationRecord
     end
 
     def reset_session_token!
-        self.session_token = SecureRandom::url_safe64
+        self.session_token = generate_unique_session_token #SecureRandom::url_safe64
         self.save!
         self.session_token
     end 
 
+    private
+
     def ensure_session_token
-        self.session_token ||= SecureRandom::url_safe64
+        self.session_token ||= generate_unique_session_token #SecureRandom::url_safe64
     end 
+
+    def generate_unique_session_token
+        loop do
+            session_token = SecureRandom::urlsafe_base64
+            return session_token unless User.exists?(session_token: session_token)
+        end
+    end
+    
 
 
 end
--- ninety-nine-cats2/config/routes.rb
+++ ninety-nine-cats2/config/routes.rb
@@ -14,4 +14,8 @@ Rails.application.routes.draw do
       post :deny
     end
   end
+
+  resources :users, only: [:new, :create]
+
+  resource :sessions, only: [:new, :create, :destroy]
 end
